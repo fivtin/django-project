b95a1ffc9d4cea87804e2dcbb34b3ccd362a0b03
fivtin <fivtin@mail.ru>
2024-05-03 13:42:21 +0300

    task 4.3 - removed field 'manufactured_at'

--- catalog/models.py
+++ catalog/models.py
@@ -31,7 +31,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    manufactured_at = models.DateTimeField(**NULLABLE)
 
     def __str__(self):
         return f'<Product: {self.title}, price: {self.price}>'

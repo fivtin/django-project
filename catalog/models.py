b493c3f407fd54d2eb68aec9048147e0365ea30a
fivtin <fivtin@mail.ru>
2024-05-03 13:33:30 +0300

    task 4.2 - added field 'manufactured_at'

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
 
+NULLABLE = {
+    'null': True,
+    'blank': True,
+}
+
+
 class Category(models.Model):
     """ A class that implements the 'category' model. """
 
@@ -20,11 +26,12 @@ class Product(models.Model):
 
     title = models.CharField(max_length=128)
     description = models.TextField()
-    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    image = models.ImageField(upload_to='images/', **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    manufactured_at = models.DateTimeField(**NULLABLE)
 
     def __str__(self):
         return f'<Product: {self.title}, price: {self.price}>'

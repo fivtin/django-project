432e1d06ee34b91ec310d9eb551cbfb628d01e1a
fivtin <fivtin@mail.ru>
2024-05-03 11:59:21 +0300

    task 2 - added Product and Category models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    """ A class that implements the 'product' model. """
+
+    def __str__(self):
+        return '<Product: >'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+
+class Category(models.Model):
+    """ A class that implements the 'category' model. """
+
+    def __str__(self):
+        return '<Category: >'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+

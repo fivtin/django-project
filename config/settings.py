e09c66cd13cd22716c7884f3f69f58a17c316c10
fivtin <fivtin@mail.ru>
2024-05-03 11:40:46 +0300

    connect to postgresql

--- config/settings.py
+++ config/settings.py
@@ -76,10 +76,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django5',
+        'USER': 'django5',
+        'PASSWORD': 'django5',
+        'HOST': 'localhost',
+    }
 }
 
 
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 

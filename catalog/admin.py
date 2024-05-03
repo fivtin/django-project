f8f8c931c102dd5b0af4cd43aff796201fa84074
fivtin <fivtin@mail.ru>
2024-05-03 16:22:09 +0300

    task 5 - setting up the admin panel

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from catalog.models import Category, Product
+
+
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', )
+    search_fields = ('title', 'description', )
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'price', )
+    list_filter = ('category__title', )
+    search_fields = ('title', 'description', )
+
--- catalog/models.py
+++ catalog/models.py
@@ -10,31 +10,33 @@ NULLABLE = {
 class Category(models.Model):
     """ A class that implements the 'category' model. """
 
-    title = models.CharField(max_length=128)
-    description = models.TextField()
+    title = models.CharField(max_length=128, verbose_name='категория')
+    description = models.TextField( verbose_name='описание')
 
     def __str__(self):
-        return f'<Category: {self.title}>'
+        return f'{self.title}'
 
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
+        ordering = ('id', )
 
 
 class Product(models.Model):
     """ A class that implements the 'product' model. """
 
-    title = models.CharField(max_length=128)
-    description = models.TextField()
-    image = models.ImageField(upload_to='images/', **NULLABLE)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=128, verbose_name='продукт')
+    description = models.TextField( verbose_name='описание')
+    image = models.ImageField(upload_to='images/', **NULLABLE,  verbose_name='превью')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,  verbose_name='категория')
+    price = models.DecimalField(max_digits=10, decimal_places=2,  verbose_name='цена')
+    created_at = models.DateTimeField(auto_now_add=True,  verbose_name='дата добавления')
+    updated_at = models.DateTimeField(auto_now=True,  verbose_name='дата изменения')
 
     def __str__(self):
-        return f'<Product: {self.title}, price: {self.price}>'
+        return f'{self.title}, цена: {self.price}'
 
     class Meta:
         verbose_name = 'продукт'
         verbose_name_plural = 'продукты'
+        ordering = ('id', )

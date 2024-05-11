3b1b44484ad25c56053e7c2e77af5aa8d6a32da4
vitt <fivtin@mail.ru>
2024-05-12 02:07:03 +0300

    task 3 - split html into templates

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
+from catalog.apps import CatalogConfig
 from catalog.views import main_page, contacts, product_detail
 
-# app_name = 'students_list'
+
+app_name = CatalogConfig.name # 'catalog'
 
 urlpatterns = [
-    path('', main_page),
+    path('', main_page, name='main_page'),
     path('product/<int:pk>', product_detail, name='product_detail'),
-    path('contacts/', contacts),
+    path('contacts/', contacts, name='contacts'),
 ]

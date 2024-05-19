24a9f5fa5620c3f76a65fac3dc34d49c67c9e03a
vitt <fivtin@mail.ru>
2024-05-20 00:22:34 +0300

    task 3 - completed

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from pytils.translit import slugify
 
@@ -10,9 +10,21 @@ from blog.models import Blog
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+
+        return queryset
+
 
 class BlogDetailView(DetailView):
     model = Blog
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
 
 
 class BlogCreateView(CreateView):
@@ -32,7 +44,6 @@ class BlogCreateView(CreateView):
 class BlogUpdateView(UpdateView):
     model = Blog
     fields = ('title', 'content', 'preview', 'is_published', )
-    success_url = reverse_lazy('blog:list')
 
     def form_valid(self, form):
         if form.is_valid():
@@ -42,6 +53,9 @@ class BlogUpdateView(UpdateView):
             new_blog.save()
 
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:detail', args=[self.kwargs.get('pk')])
 
 
 class BlogDeleteView(DeleteView):

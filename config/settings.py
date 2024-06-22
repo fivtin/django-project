db8ea755234ca9da06fd4e5475ee8e750b403d27
vitt <fivtin@mail.ru>
2024-06-22 20:59:37 +0300

    HW 23.2 - completed task 1 - install and setup redis

--- config/settings.py
+++ config/settings.py
@@ -152,3 +152,11 @@ EMAIL_USE_SSL = True
 
 SERVER_EMAIL = EMAIL_HOST_USER
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+CACHE_ENABLED = True
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": os.getenv("REDIS_LOCATION"),
+    }
+}
